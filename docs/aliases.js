5bcd7bd4330df8165edee4962e2f9d1b8f59cd5b
Eugene P <mahou@shoujo.pw>
2019-10-05 19:33:41 +0300

    Update pregen docs

--- docs/aliases.js
+++ docs/aliases.js
@@ -1,2 +1,2 @@
 var ALIASES = {};
-ALIASES['growable'] = {};
+ALIASES["growable"] = {};
--- docs/implementors/core/ops/deref/trait.Deref.js
+++ docs/implementors/core/ops/deref/trait.Deref.js
@@ -6,5 +6,4 @@ implementors["growable"] = [{text:"impl&lt;T:&nbsp;?<a class=\"trait\" href=\"ht
             } else {
                 window.pending_implementors = implementors;
             }
-        
-})()
+        })()
