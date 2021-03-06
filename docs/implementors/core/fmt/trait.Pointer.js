0644648fe2f694fa664ce3888d9b2a1209bfcab5
Eugene P <mahou@shoujo.pw>
2021-03-06 16:24:51 +0300

    Update for rustc 1.52.0-nightly (caca2121f 2021-03-05) (#16)
    * Update versions
    
    * Update for rustc 1.52.0-nightly (caca2121f 2021-03-05)
    
    * Update license
    
    * Update docs
    
    * Document new feature

--- docs/implementors/core/fmt/trait.Pointer.js
+++ docs/implementors/core/fmt/trait.Pointer.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
-implementors["growable"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"growable/struct.Growable.html\" title=\"struct growable::Growable\">Growable</a>",synthetic:false,types:["growable::Growable"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"growable/struct.Reusable.html\" title=\"struct growable::Reusable\">Reusable</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: ?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>,&nbsp;</span>",synthetic:false,types:["growable::Reusable"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+implementors["growable"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"growable/struct.Growable.html\" title=\"struct growable::Growable\">Growable</a>","synthetic":false,"types":["growable::Growable"]},{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"growable/struct.Reusable.html\" title=\"struct growable::Reusable\">Reusable</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: ?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>,&nbsp;</span>","synthetic":false,"types":["growable::Reusable"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
